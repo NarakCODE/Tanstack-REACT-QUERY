244a37f954e0f81e3fe16ba110c9b2c02d4c9f1a
NarakCODE <channarakluy@gmail.com>
2025-01-03 14:26:45 +0700

    Complete project 1

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,14 +1,21 @@
-import { Posts } from "./Posts";
-import "./App.css";
+import { Posts } from './Posts';
+import './App.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const client = new QueryClient();
 
 function App() {
-  return (
-    // provide React Query client to App
-    <div className="App">
-      <h1>Blog &apos;em Ipsum</h1>
-      <Posts />
-    </div>
-  );
+    return (
+        // provide React Query client to App
+        <QueryClientProvider client={client}>
+            <div className='App'>
+                <h1>Blog &apos;em Ipsum</h1>
+                <Posts />
+                <ReactQueryDevtools />
+            </div>
+        </QueryClientProvider>
+    );
 }
 
 export default App;
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,40 +1,87 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react';
 
-import { fetchPosts, deletePost, updatePost } from "./api";
-import { PostDetail } from "./PostDetail";
+import { fetchPosts, deletePost, updatePost } from './api';
+import { PostDetail } from './PostDetail';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 const maxPostPage = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
-  const [selectedPost, setSelectedPost] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+    const deleteMutation = useMutation({
+        mutationFn: postId => deletePost(postId),
+    });
 
-  return (
-    <>
-      <ul>
-        {data.map((post) => (
-          <li
-            key={post.id}
-            className="post-title"
-            onClick={() => setSelectedPost(post)}
-          >
-            {post.title}
-          </li>
-        ))}
-      </ul>
-      <div className="pages">
-        <button disabled onClick={() => {}}>
-          Previous page
-        </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
-          Next page
-        </button>
-      </div>
-      <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
-    </>
-  );
+    const updateMutation = useMutation({
+        mutationFn: postId => updatePost(postId),
+    });
+
+    const queryClient = useQueryClient();
+    useEffect(() => {
+        if (currentPage < maxPostPage) {
+            const nextPage = currentPage + 1;
+            queryClient.prefetchQuery({
+                queryKey: ['posts', nextPage],
+                queryFn: () => fetchPosts(nextPage),
+            });
+        }
+    }, [currentPage, queryClient]);
+
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ['posts', currentPage],
+        queryFn: () => fetchPosts(currentPage),
+        staleTime: 2000,
+    });
+
+    if (isLoading) return <h3>Loading...</h3>;
+
+    if (isError) return <h3>Error</h3>;
+
+    return (
+        <>
+            <ul>
+                {data.map(post => (
+                    <li
+                        key={post.id}
+                        className='post-title'
+                        onClick={() => {
+                            deleteMutation.reset();
+                            updateMutation.reset();
+                            setSelectedPost(post);
+                        }}
+                    >
+                        {post.title}
+                    </li>
+                ))}
+            </ul>
+            <div className='pages'>
+                <button
+                    disabled={currentPage <= 1}
+                    onClick={() => {
+                        setCurrentPage(prev => prev - 1);
+                    }}
+                >
+                    Previous page
+                </button>
+                <span>Page {currentPage}</span>
+                <button
+                    disabled={currentPage >= maxPostPage}
+                    onClick={() => {
+                        setCurrentPage(prev => prev + 1);
+                    }}
+                >
+                    Next page
+                </button>
+            </div>
+            <hr />
+            {selectedPost && (
+                <PostDetail
+                    post={selectedPost}
+                    deleteMutation={deleteMutation}
+                    updateMutation={updateMutation}
+                />
+            )}
+        </>
+    );
 }
